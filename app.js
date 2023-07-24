0649c360d4fd13180efae5cb1fb8ffb02481469f
Yuvraj <yuvraj@YMA.local>
2023-07-24 12:05:47 +0530

    Unsuccefull
    Not Worked

--- app.js
+++ app.js
@@ -1,64 +1,53 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const bodyParser = require('body-parser');
 
-const { MongoClient } = require("mongodb");
+const app = express();
+const port = 3000;
 
-async function run() {
-  // TODO:
-  // Replace the placeholder connection string below with your
-  // Altas cluster specifics. Be sure it includes
-  // a valid username and password! Note that in a production environment,
-  // you do not want to store your password in plain-text here.
-  const uri =
-    "mongodb+srv://Aruna001:xoTPhDlsWhF1wLJV@cluster0.jl4iorb.mongodb.net/?retryWrites=true&w=majority";
+// MongoDB connection URL - Replace with your MongoDB Atlas connection string
+const mongoURI =
+  "mongodb+srv://Aruna001:xoTPhDlsWhF1wLJV@cluster0.jl4iorb.mongodb.net/myDatabase?retryWrites=true&w=majority";
 
-  // The MongoClient is the object that references the connection to our
-  // datastore (Atlas, for example)
-  const client = new MongoClient(uri);
-
-  // The connect() method does not attempt a connection; instead it instructs
-  // the driver to connect using the settings provided when a connection
-  // is required.
-  await client.connect();
-
-  // Provide the name of the database and collection you want to use.
-  // If the database and/or collection do not exist, the driver and Atlas
-  // will create them automatically when you first write data.
-  const dbName = "myDatabase";
-  const collectionName = "users";
-
-  // Create references to the database and collection in order to run
-  // operations on them.
-  const database = client.db(dbName);
-  const collection = database.collection(collectionName);
-
-  const users = [
-    
-      
-      
-    
-  ];
-  
-  try {
-    const insertManyResult = await collection.insertMany(users);
-    console.log(`${insertManyResult.insertedCount} documents successfully inserted.\n`);
-  } catch (err) {
-    console.error(`Something went wrong trying to insert the new documents: ${err}\n`);
+// Connect to MongoDB
+MongoClient.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+  if (err) {
+    console.log('Error connecting to MongoDB:', err);
+    return;
   }
 
-  const deleteQuery = { name: { $in: ["elotes"] } };
-  try {
-    const deleteResult = await collection.deleteMany(deleteQuery);
-    console.log(`Deleted ${deleteResult.deletedCount} documents\n`);
-  } catch (err) {
-    console.error(`Something went wrong trying to delete documents: ${err}\n`);
-  }
+  // Access the database
+  const db = client.db();
 
-  // Make sure to call close() on your client to perform cleanup operations
-  await client.close();
+  // Middleware to parse incoming form data
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-  
-}
+  // Define a route to serve your HTML page
+  app.get('/', (req, res) => {
+    res.sendFile("/Users/yuvraj/Downloads/atlas_starter_nodejs-master/client/index.html");
+  });
 
+  // Define a route to handle form submissions and save data to MongoDB
+  app.post('/submit', (req, res) => {
+    const { name, email } = req.body;
 
-run().catch(console.dir);
+    // Save the data to the MongoDB collection
+    const collection = db.collection('users'); // Replace 'users' with your desired collection name
+    collection.insertOne({ name, email }, (err, result) => {
+      if (err) {
+        console.log('Error saving data to MongoDB:', err);
+        res.status(500).send('Error saving data to MongoDB');
+        return;
+      }
 
+      console.log('Data saved to MongoDB:', result.ops);
+      // Redirect the user to a new page after successful form submission
+      res.redirect('/success.html');
+    });
+  });
 
+  // Start the server
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+});
